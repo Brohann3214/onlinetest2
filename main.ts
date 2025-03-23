40eb31bda4dc6911d149524ae8f170897b5cbb25
Brohann <129230434+Brohann3214@users.noreply.github.com>
2025-03-23 15:10:59 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,7 +2,7 @@ namespace SpriteKind {
     export const hitbox = SpriteKind.create()
 }
 controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
-    if (playerid ==  1){
+    if (playerid == 1) {
         if (mySprite.isHittingTile(CollisionDirection.Bottom) || myhitbox.overlapsWith(mySprite2)) {
             mySprite.vy = -170
         }
@@ -12,15 +12,26 @@ controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
         }
     }
 })
+controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
+    playerid = 2
+})
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    playerid = 1
+})
+let p2y2 = 0
+let p2x2 = 0
+let p2y = 0
+let p2x = 0
 let mySprite2: Sprite = null
 let myhitbox: Sprite = null
 let mySprite: Sprite = null
-let playerid = 1
-let gottenanswer = false
-let connection = false
-let answer = ''
-let datareq = ""
+let playerid = 0
 let req = ""
+let datareq = ""
+let answer = ''
+let connection = false
+let gottenanswer = false
+playerid = 1
 let testmsg = "tick"
 const ws = new WebSocket("wss://weboscketserver2.onrender.com")
 control.runInParallel(function () {
@@ -53,7 +64,6 @@ control.runInParallel(function () {
 
 
 })
-
 scene.setBackgroundColor(13)
 tiles.setCurrentTilemap(tilemap`level`)
 mySprite = sprites.create(img`
@@ -115,57 +125,58 @@ mySprite2 = sprites.create(img`
     8 6 6 6 6 6 6 6 6 6 6 6 6 6 6 8 
     . 8 8 8 8 8 8 8 8 8 8 8 8 8 8 . 
     `, SpriteKind.Player)
-
-
 scene.cameraFollowSprite(mySprite)
 CollisionHandler.handleSolidCollision(mySprite, SpriteKind.Player)
-controller.B.onEvent(ControllerButtonEvent.Pressed, function() {
-    playerid = 2
-})
 game.onUpdate(function () {
-    myhitbox.setPosition(mySprite.x, mySprite.y)
-    if (playerid == 1){
+    if (playerid == 1) {
+        myhitbox.setPosition(mySprite.x, mySprite.y)
         controller.moveSprite(mySprite, 100, 0)
         mySprite.ay = 500
         controller.moveSprite(mySprite2, 0, 0)
         mySprite2.ay = 0
-    } else{
+        scene.cameraFollowSprite(mySprite)
+    } else {
         controller.moveSprite(mySprite, 0, 0)
         mySprite.ay = 0
         mySprite2.ay = 500
         controller.moveSprite(mySprite2, 100, 0)
+        scene.cameraFollowSprite(mySprite2)
+        myhitbox.setPosition(mySprite2.x, mySprite2.y)
     }
     if (playerid == 1 && gottenanswer) {
-        datareq = "n/mmop/player1/x>" +  mySprite.x
+        datareq = "n/mmop/player1/x>" + mySprite.x
         ws.send(datareq)
-        datareq = "n/mmop/player1/y>" + mySprite.y
+datareq = "n/mmop/player1/y>" + mySprite.y
         ws.send(datareq)
-        datareq = "/mmop/player2/x"
+datareq = "/mmop/player2/x"
         gottenanswer = false
         ws.send(datareq)
-        pauseUntil(() => gottenanswer)
-        let p2x = parseInt(answer)
+pauseUntil(() => gottenanswer)
+        p2x = parseInt(answer)
         datareq = "/mmop/player2/y"
         gottenanswer = false
         ws.send(datareq)
-        pauseUntil(() => gottenanswer)
-        let p2y = parseInt(answer)
-        mySprite2.setPosition(p2x,p2y)
-    } else if (connection){
+pauseUntil(() => gottenanswer)
+        p2y = parseInt(answer)
+        mySprite2.setPosition(p2x, p2y)
+    } else if (connection) {
         datareq = "n/mmop/player2/x>" + mySprite.x
         ws.send(datareq)
-        datareq = "n/mmop/player2/y>" + mySprite.y
+datareq = "n/mmop/player2/y>" + mySprite.y
         ws.send(datareq)
-        datareq = "/mmop/player1/x"
+datareq = "/mmop/player1/x"
         gottenanswer = false
         ws.send(datareq)
-        pauseUntil(() => gottenanswer)
-        let p2x = parseInt(answer)
+pauseUntil(() => gottenanswer)
+        p2x2 = parseInt(answer)
         datareq = "/mmop/player1/y"
         gottenanswer = false
         ws.send(datareq)
-        pauseUntil(() => gottenanswer)
-        let p2y = parseInt(answer)
-        mySprite.setPosition(p2x, p2y)
+pauseUntil(() => gottenanswer)
+        p2y2 = parseInt(answer)
+        mySprite.setPosition(p2x2, p2y2)
     }
+})
+game.onUpdate(function () {
+	
 })
